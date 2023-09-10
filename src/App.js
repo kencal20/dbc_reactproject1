f92805476e60d9a0f1594b76c44722c50ffbbd36
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-09-10 11:58:51 +0000

    appying container to class from the first section to other sections

--- src/App.js
+++ src/App.js
@@ -7,17 +7,38 @@ function App() {
     <div className="App">
       <div className='container'>
         <img src={logo} alt='kate' />
-        <hr/>
+        <hr />
         <section>
-      <h3>
-        Katherine Johnson
-      </h3>
-      <hr width ='400px'/>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime debitis eum repellendus modi similique ipsa. Beatae vitae similique aperiam accusantium temporibus eligendi aliquam delectus cumque veniam praesentium? Dolor, incidunt minus!</p>
+          <h3>
+            Katherine Johnson
+          </h3>
+          <hr width='400px' />
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime debitis eum repellendus modi similique ipsa. Beatae vitae similique aperiam accusantium temporibus eligendi aliquam delectus cumque veniam praesentium? Dolor, incidunt minus!</p>
         </section>
       </div>
-  
-  
+
+      <div className='container'>
+        <section>
+          <h3>
+            Queen ELizabeth
+          </h3>
+          <hr width='400px' />
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime debitis eum repellendus modi similique ipsa. Beatae vitae similique aperiam accusantium temporibus eligendi aliquam delectus cumque veniam praesentium? Dolor, incidunt minus!</p>
+        </section>
+        <hr />
+        <img src={logo} alt='kate' />
+      </div>
+      <div className='container'>
+        <img src={logo} alt='kate' />
+        <hr />
+        <section>
+          <h3>
+            Kwame Nkrumah
+          </h3>
+          <hr width='400px' />
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime debitis eum repellendus modi similique ipsa. Beatae vitae similique aperiam accusantium temporibus eligendi aliquam delectus cumque veniam praesentium? Dolor, incidunt minus!</p>
+        </section>
+      </div>
     </div>
   );
 }
