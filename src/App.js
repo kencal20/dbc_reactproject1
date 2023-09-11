4c1743a76366d467d55a393f2af6ec38efc1cfa7
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-09-11 12:04:22 +0000

    removal of profiles pertaining to historical figures and implementing of .map function

--- src/App.js
+++ src/App.js
@@ -1,46 +1,65 @@
-import nkrumah from './DrNkrumah.jpg'
-import queen from './queenElizabeth.jpg'
-import './styles.css'
 import './App.css';
-import Profile1 from "./Components/App.profile"
-import { Profile2 } from './Components/App.profile';
+
+// import Profile from './Components/Profile';
+import Item from './Components/item';
+
+
 
 
 function App() {
+
+  let todos = [
+    {
+      todo: "Cloths Arranging",
+      status: true
+    },
+
+    {
+      todo: "Eating Lunch",
+      status: false
+    },
+
+    {
+      todo: "Clean Room",
+      status: false
+    },
+
+    {
+      todo: "Painting garage",
+      status: true
+    },
+
+    {
+      todo: "Beauty Sleep",
+      status: true
+    }
+  ]
+
+  let elements = todos.map((todoItem) => {
+    return <Item todo={todoItem.todo} />
+  }
+  )
+
+
   return (
     <div className="App">
-      <br />
-      <Profile1
-        image="kate.jpg"
-        alt="a pic of kate"
-        name="Kate"
-        description=" 
-            Creola Katherine Johnson (née Coleman; August 26, 1918 – February 24, 2020) was an American mathematician whose calculations of orbital mechanics as a NASA employee were critical to the success of the first and subsequent U.S. crewed spaceflights.
-              During her 33-year career at NASA and its predecessor, she earned a reputation for mastering complex manual calculations and helped pioneer the use of computers to perform the tasks.
-              The space agency noted her historical role as one of the first African-American women to work as a NASA scientist "
-        link="jhghjg"
+      {/* <Profile /> */}
+      <h2>Todo list</h2>
+      <Item
+        todo="Shopping for clothes"
+        status={true}
+      />
+      <Item
+        todo="Wash your clothes"
+        status={false}
       />
 
-      <Profile2
-        image={queen}
-        alt="a pic of kate"
-        name="Queen Elizabeth"
-        description=" 
-      Elizabeth II was Queen of the United Kingdom and other Commonwealth realms from 6 February 1952 until her death in 2022.
-       She was queen regnant of 32 sovereign states over the course of her lifetime and remained the monarch of 15 realms by the time of her death."
-        link='kalsdj'
+      <Item
+        todo="Going on dinner Date"
+        status={false}
       />
+    </div>
 
-
-      <Profile1
-        image={nkrumah}
-        alt="Kwame Nkrumah"
-        name="Kwame Nkrumah"
-        description="Francis Kwame Nkrumah was a Ghanaian politician, political theorist, and revolutionary.
-         He was the first Prime Minister and President of Ghana, having led the Gold Coast to independence from Britain in 1957."
-      link='asdjalskd'
-  />
-</div>
 
   );
 }
