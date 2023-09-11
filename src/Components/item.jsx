3f5f54cf2037105d2c998d69c9e30b14b4888255
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-09-11 12:01:13 +0000

    using if statement to show todo list

---
+++ src/Components/item.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Item({ todo, status }) {
+  if (status) {
+    return (
+      <div className="container">
+        <p>
+          {" "}
+          {todo} <span> : completed</span>{" "}
+        </p>
+      </div>
+    );
+  } 
+  // else {
+  //   return (
+  //     <div className="container">
+  //       <p>
+  //         {todo}
+  //         <span> : pending</span>
+  //       </p>
+  //     </div>
+  //   );
+  // }
+}
