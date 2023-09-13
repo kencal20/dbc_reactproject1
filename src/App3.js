b2c1a646061fc530246b843259c42f8ccf2226b0
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-09-13 11:50:30 +0000

    app3 init

---
+++ src/App3.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Gallery from './Components/gallery'
+
+export default function App3() {
+  return (
+    <div className='App3'>
+      <Gallery/>
+    </div>
+  )
+}
