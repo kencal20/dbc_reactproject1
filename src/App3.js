ce576ec16aa5591ae5280c53314d4b737d3c05ce
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-09-13 18:24:31 +0000

    updating the gallery Elemnt

--- src/App3.js
+++ src/App3.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import { React, useState } from 'react'
 import Gallery from './Components/gallery'
 
 export default function App3() {
+  const [index, setIndex] = useState(1)
+  function onNextClick() {
+    setIndex((prevIndex) => { return prevIndex + 1 })
+  }
   return (
     <div className='App3'>
-      <Gallery/>
+      <Gallery 
+      index ={index}
+      onNextClicked ={onNextClick}
+      />
+
     </div>
   )
 }
