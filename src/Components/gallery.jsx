78f1e940206929cdca979c1ac7426b0c2f9ce57a
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-09-13 11:50:43 +0000

    gallery init

---
+++ src/Components/gallery.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Gallery() {
+  return (
+    <div>
+      <button>Next</button>
+      <h2>
+        {" "}
+        <i>()</i> by {}
+      </h2>
+      <h3>()</h3>
+      <img src={} 
+        alt={}
+       />
+       <p>
+        {}
+       </p>
+    </div>
+  );
+}
+
+export default Gallery;
