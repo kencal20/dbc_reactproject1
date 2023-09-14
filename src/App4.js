060d44990ac0a52a37ad09d7c2bf04036dda5a48
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-09-14 10:14:14 +0000

    changing the element Item variable from being a local to global variable

--- src/App4.js
+++ src/App4.js
@@ -5,10 +5,9 @@ export default function App4() {
     const [item, setItem] = useState(['item1', 'item2'])
     function addItemOnUpdate() {
         setItem((prevItem) => { return [...prevItem, `item${prevItem.length + 1}`] })
-       
-        let elementItem = item.map((item) => { return <p key={item.length + 1}></p> })
 
     }
+    let elementItem = item.map((item) => { return <p key={item.length + 1}>{item}</p> })
 
     return (
         <div className='App4'>
@@ -16,7 +15,7 @@ export default function App4() {
                 add item
             </button>
             {
-                item
+                elementItem
             }
         </div>
     )
