9f5ba2d5421aee368b59897b272026bbd96d1f30
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-09-14 09:44:05 +0000

    rendering app4 index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
 import App3 from './App3';
+import App4 from './App4'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App3 />
+    <App4 />
   </React.StrictMode>
 );
 
