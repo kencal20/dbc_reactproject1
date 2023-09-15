3e851809dc06b7b4a89d2bcfbbfb29c6649d31a3
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-09-15 09:26:49 +0000

    contactCard initialization

---
+++ src/App5.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react'
+
+export default function App5() {
+
+    const[position,setPosition] =useState()
+    return (
+        <div>
+
+    <h1>Hello</h1>
+            
+            
+            </div>
+    )
+}
